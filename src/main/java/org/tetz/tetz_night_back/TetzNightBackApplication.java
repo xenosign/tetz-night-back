a4902abbf2f3b555df431fd8fbf2a98612eacfa8
kdtTetz <xenosign@naver.com>
2025-01-12 04:35:01 +0900

    250112 fc

---
+++ src/main/java/org/tetz/tetz_night_back/TetzNightBackApplication.java
@@ -0,0 +1,13 @@
+package org.tetz.tetz_night_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetzNightBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetzNightBackApplication.class, args);
+	}
+
+}
