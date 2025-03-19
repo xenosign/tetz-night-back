5f827888b633d35344b8661dfb578049ba6b075c
Tetz <xenosign@naver.com>
2025-03-19 10:21:52 +0900

    Update HomeController.java

--- src/main/java/org/tetz/tetz_night_back/controller/HomeController.java
+++ src/main/java/org/tetz/tetz_night_back/controller/HomeController.java
@@ -2,6 +2,7 @@ package org.tetz.tetz_night_back.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,5 +12,10 @@ public class HomeController {
     @GetMapping("")
     public String vote() {
         return "Hello, world!";
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("여기는 Tetz 백엔드 서버입니다");
     }
 }
